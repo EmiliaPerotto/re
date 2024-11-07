e8dcac34482b6d5ef85fd05a048bfcc375687666
Emilia Perotto <100068661@csisd.org>
2024-11-07 14:12:03 -0600

    hi

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# re
+Create a program called RecursiveMethods.java
+
+Create a static method that takes an integer n as input and calculates n factorial
+
+fact(n) = n * (n-1) * (n-2) * ... * 2 * 1
+
+Create a static method that takes an integer n as input and calculates the n-th number in the Fibonacci sequence
+
+fib(n) = fib(n-1) + fib(n-2), fib(0) = 0 and fib(1) = 1
+
+You need to figure out what your base case should be
+
+Create a static isOdd method that takes an integer n as input and returns true or false based on whether n is odd or not
+
+You must calculate this recursively, no modulus
+
+Inside the main method
+
+Prompt the user for a number and call the factorial method with that number as a parameter
+
+Prompt the user for a number and call the fibonacci method with that number as a parameter
+
+Prompt the user for a number and call the isOdd method with that number as a parameter
+
+Print the result of the factorial method
+
+Print the result fibonacci method
+
+Print "even" or "odd" depending on the result of the isOdd method
+
+Example Input:
+4
+4
+4
+Example Output:
+24
+3
+even
---
+++ RecursiveMethods.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class RecursiveMethods{
+  public RecursiveMethods(){//this is the default constructor
+
+  }
+    public static int factSequence(int n) {
+      if (n == 0) {//this will calculate the factorial sequenc
+        return 0;
+      } 
+      else {
+        return n * factSequence(n-1);
+      }
+  }
+    public static int fibSequence(int n) {
+      if (n <= 1) {//this will calculate the fibonnaci sequence
+        return 1;
+      } else {
+        return fibSequence(n-1) + fibSequence(n-2);
+      }
+    }
+    public static boolean isOdd(int n){
+      if (n%2 == 0){//calculate using recursive method do not use modulus, this will also calculate whether n is even/odd
+        return true;
+      }
+      else{
+        return false;
+      }
+
+    }
+  
+  public static void main(String args[]){
+    Scanner in = new Scanner(System.in);
+    System.out.println("Input number: ");
+    int factMethod = Integer.parseInt(in.nextLine());
+    System.out.println("Input number: ");
+    int fibMethod = Integer.parseInt(in.nextLine());
+    System.out.println("Input number: ");
+    int isOddMethod = Integer.parseInt(in.nextLine());
+    System.out.println(RecursiveMethods.factSequence(factMethod));
+    System.out.println(RecursiveMethods.fibSequence(fibMethod));
+    System.out.println(RecursiveMethods.isOdd(isOddMethod));
+
+}
+}
